2c6eed4a00c14df98720f53edb1d350d79fc085f
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2018-04-16 10:20:47 +0200

    re-initialize migration

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -8,7 +8,6 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0025_auto_20170228_0547'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -20,11 +19,9 @@ class Migration(migrations.Migration):
                 ('title', models.CharField(max_length=300)),
                 ('description', models.TextField()),
                 ('image', models.ImageField(null=True, upload_to=b'news_updates', blank=True)),
-                ('publish_date', models.DateTimeField()),
                 ('date_created', models.DateTimeField(auto_now_add=True)),
                 ('date_updated', models.DateTimeField(auto_now=True)),
                 ('publisher', models.ForeignKey(related_name='news', to=settings.AUTH_USER_MODEL)),
-                ('resource', models.ForeignKey(to='base.ResourceBase')),
             ],
             options={
                 'db_table': 'cartoview_news',
